05de19171fdcb00337c2682f1c2cd69d39a0da31
Jere Pihlajasaari <jere.pihlajasaari@hotmail.com>
2024-02-10 18:22:44 +0200

    Acorn collect sound & jump sound changed
    Added a collecting sound for acorn and changed the sound for jumping

--- runfoxrun/Assets/Scripts/Collectables/CollectAcorn.cs
+++ runfoxrun/Assets/Scripts/Collectables/CollectAcorn.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectAcorn : MonoBehaviour {
+
+    public AudioSource AcornSound;
+
     // Start is called before the first frame update
     void OnTriggerEnter( Collider other ) {
         UIControl.acornCount += 1;
         this.gameObject.SetActive(false);
+        AcornSound.Play();
     }
 }
